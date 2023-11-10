3b5fd41c3ba6583c0d511c890a1eb69473754b25
lod <djumicbranislav@outlook.com>
2023-11-10 20:56:06 +0100

    Reordered methods by grouping them by ObjType

--- src/object.c
+++ src/object.c
@@ -27,6 +27,95 @@ static Obj *allocateObject(size_t size, ObjType type) {
     return object;
 }
 
+void printObject(Value value) {
+    AS_OBJ(value)->vtp->print(AS_OBJ(value));
+}
+
+static void printFunctionImpl(ObjFunction *function);
+
+ObjBoundMethod *newBoundMethod(Value receiver, Obj *method) {
+    ObjBoundMethod *bound = ALLOCATE_OBJ(ObjBoundMethod, OBJ_BOUND_METHOD);
+    bound->receiver = receiver;
+    bound->method = method;
+    return bound;
+}
+
+static void printObjBoundMethod(Obj *obj) {
+    Obj *method = ((ObjBoundMethod *) obj)->method;
+    ObjFunction *fun = method->type == OBJ_FUNCTION
+                       ? (ObjFunction *) method
+                       : ((ObjClosure *) method)->function;
+    printFunctionImpl(fun);
+}
+
+ObjClass *newClass(ObjString *name) {
+    ObjClass *klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
+    klass->name = name;
+    klass->initializer = NIL_VAL;
+    initTable(&klass->methods);
+    initTable(&klass->staticMethods);
+    return klass;
+}
+
+static void printObjClass(Obj *obj) {
+    printf("<class %s>", ((ObjClass *) obj)->name->chars);
+}
+
+ObjClosure *newClosure(ObjFunction *function) {
+    ObjUpvalue **upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
+
+    for (int i = 0; i < function->upvalueCount; i++) {
+        upvalues[i] = NULL;
+    }
+
+    ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
+    closure->function = function;
+    closure->upvalues = upvalues;
+    closure->upvalueCount = function->upvalueCount;
+    return closure;
+}
+
+static void printObjClosure(Obj *obj) {
+    printFunctionImpl(((ObjClosure *) obj)->function);
+}
+
+ObjFunction *newFunction() {
+    ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+    function->arity = 0;
+    function->upvalueCount = 0;
+    function->name = NULL;
+    initChunk(&function->chunk);
+    return function;
+}
+
+static void printObjFunction(Obj *obj) {
+    printFunctionImpl(((ObjFunction *) obj));
+}
+
+ObjInstance *newInstance(ObjClass *klass) {
+    ObjInstance *instance = ALLOCATE_OBJ(ObjInstance, OBJ_INSTANCE);
+    instance->klass = klass;
+    initTable(&instance->fields);
+    return instance;
+}
+
+static void printObjInstance(Obj *obj) {
+    ObjInstance *instance = (ObjInstance *) obj;
+    printf("<instance %s>", instance->klass->name->chars);
+}
+
+ObjNative *newNative(NativeFn function, int arity) {
+    ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+    native->function = function;
+    native->arity = arity;
+    return native;
+}
+
+static void printObjNative(Obj *obj) {
+    // TODO see if this can be done in a way that displays more information
+    printf("<native fn>");
+}
+
 static ObjString *allocateString(char *chars, int length,
                                  uint32_t hash) {
     ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
@@ -48,60 +137,6 @@ uint32_t hashString(const char *key, int length) {
     return hash;
 }
 
-
-ObjBoundMethod *newBoundMethod(Value receiver, Obj *method) {
-    ObjBoundMethod *bound = ALLOCATE_OBJ(ObjBoundMethod, OBJ_BOUND_METHOD);
-    bound->receiver = receiver;
-    bound->method = method;
-    return bound;
-}
-
-ObjClass *newClass(ObjString *name) {
-    ObjClass *klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
-    klass->name = name;
-    klass->initializer = NIL_VAL;
-    initTable(&klass->methods);
-    initTable(&klass->staticMethods);
-    return klass;
-}
-
-ObjClosure *newClosure(ObjFunction *function) {
-    ObjUpvalue **upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
-
-    for (int i = 0; i < function->upvalueCount; i++) {
-        upvalues[i] = NULL;
-    }
-
-    ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
-    closure->function = function;
-    closure->upvalues = upvalues;
-    closure->upvalueCount = function->upvalueCount;
-    return closure;
-}
-
-
-ObjFunction *newFunction() {
-    ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
-    function->arity = 0;
-    function->upvalueCount = 0;
-    function->name = NULL;
-    initChunk(&function->chunk);
-    return function;
-}
-
-ObjInstance *newInstance(ObjClass *klass) {
-    ObjInstance *instance = ALLOCATE_OBJ(ObjInstance, OBJ_INSTANCE);
-    instance->klass = klass;
-    initTable(&instance->fields);
-    return instance;
-}
-
-ObjNative *newNative(NativeFn function, int arity) {
-    ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
-    native->function = function;
-    native->arity = arity;
-    return native;
-}
 
 ObjString *takeString(char *chars, int length) {
     uint32_t hash = hashString(chars, length);
@@ -127,6 +162,10 @@ ObjString *copyString(const char *chars, int length) {
     return allocateString(heapChars, length, hash);
 }
 
+static void printObjString(Obj *obj) {
+    printf("%s", ((ObjString *) obj)->chars);
+}
+
 ObjUpvalue *newUpvalue(Value *slot) {
     ObjUpvalue *upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
     upvalue->location = slot;
@@ -135,8 +174,9 @@ ObjUpvalue *newUpvalue(Value *slot) {
     return upvalue;
 }
 
-void printObject(Value value) {
-    AS_OBJ(value)->vtp->print(AS_OBJ(value));
+static void printObjUpvalue(Obj *obj) {
+    // TODO see if this can be done in a way that displays more information
+    printf("upvalue");
 }
 
 static void printFunctionImpl(ObjFunction *function) {
@@ -145,45 +185,6 @@ static void printFunctionImpl(ObjFunction *function) {
         return;
     }
     printf("<fn %s>", function->name->chars);
-}
-
-static void printObjBoundMethod(Obj *obj) {
-    Obj *method = ((ObjBoundMethod *) obj)->method;
-    ObjFunction *fun = method->type == OBJ_FUNCTION
-                       ? (ObjFunction *) method
-                       : ((ObjClosure *) method)->function;
-    printFunctionImpl(fun);
-}
-
-static void printObjClass(Obj *obj) {
-    printf("<class %s>", ((ObjClass *) obj)->name->chars);
-}
-
-static void printObjClosure(Obj *obj) {
-    printFunctionImpl(((ObjClosure *) obj)->function);
-}
-
-static void printObjFunction(Obj *obj) {
-    printFunctionImpl(((ObjFunction *) obj));
-}
-
-static void printObjInstance(Obj *obj) {
-    ObjInstance *instance = (ObjInstance *) obj;
-    printf("<instance %s>", instance->klass->name->chars);
-}
-
-static void printObjNative(Obj *obj) {
-    // TODO see if this can be done in a way that displays more information
-    printf("<native fn>");
-}
-
-static void printObjString(Obj *obj) {
-    printf("%s", ((ObjString *) obj)->chars);
-}
-
-static void printObjUpvalue(Obj *obj) {
-    // TODO see if this can be done in a way that displays more information
-    printf("upvalue");
 }
 
 static ObjVT vtList[] = {
