0b30be1366bc2cf6ab57a2e07874e7dd02ebee40
lordofdestiny <djumicbranislav@outlook.com>
2026-01-28 10:58:31 +0000

    Use stable jansson version

--- cmake/FetchJannson.cmake
+++ cmake/FetchJannson.cmake
@@ -1,10 +1,9 @@
 include(FetchContent)
 
-message(AUTHOR_WARNING "Once new version of jansson merges PR with cmake fixes, move to that tag")
 FetchContent_Declare(
   jansson
   GIT_REPOSITORY https://github.com/akheron/jansson
-  GIT_TAG        154f8919fb0d3778282b797789ad985e3d99fdc8  # Use the latest stable version
+  GIT_TAG        v2.15.0  # Use the latest stable version
 )
 
 set(JANSSON_BUILD_DOCS OFF CACHE BOOL "Don't build Jansson docs" FORCE)
