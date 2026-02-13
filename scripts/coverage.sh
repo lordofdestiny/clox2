fbe2a1cef2240da350b17fe892e0a7e0c5afca66
lordofdestiny <djumicbranislav@outlook.com>
2026-02-13 21:42:55 +0000

    Fix presets

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,16 +3,15 @@
 set -ex
 source .venv/bin/activate
 
-cmake --workflow --preset build-and-coverage "$@"
+cmake --workflow --preset dev-build-coverage "$@"
 
 # Check if the directory exists before creating it
 if [ ! -d "coverage/html" ]; then
     mkdir -p coverage/html
 fi
 
-python -m gcovr -r . -e ".*\.h" -e src/impl/src -e build-cov/_deps/ -e test/ \
+python -m gcovr -r . -e ".*\.h" -e src/impl/src -e coverage/_deps/ -e test/ \
     --html-details coverage/html/index.html \
     --markdown coverage/coverage.md \
-    --markdown-summary=coverage/summary.md
 
 echo "Coverage report generated."
